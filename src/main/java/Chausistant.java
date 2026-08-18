58f0adbb9259920f0c98df2059e47a5de81d8167
chausarahnusitems-tech <chausarah.nusitems@gmail.com>
2026-08-18 16:28:50 +0800

    Level-0: ASCII banner and changed name to chausistant

---
+++ src/main/java/Chausistant.java
@@ -0,0 +1,19 @@
+/**
+ * Entry point for the Chausistant chatbot application.
+ */
+public class Chausistant {
+    public static void main(String[] args) {
+        String banner = """
+                 ████ █   █  ███  █   █  ████ █████  ████ █████  ███  █   █ █████
+                █     █   █ █   █ █   █ █       █   █       █   █   █ ██  █   █
+                █     █   █ █   █ █   █ █       █   █       █   █   █ ██  █   █
+                █     █████ █████ █   █  ███    █    ███    █   █████ █ █ █   █
+                █     █   █ █   █ █   █    █    █      █    █   █   █ █  ██   █
+                █     █   █ █   █ █   █    █    █      █    █   █   █ █  ██   █
+                 ████ █   █ █   █  ███  ████  █████ ████    █   █   █ █   █   █
+
+                                      chausistant
+                """;
+        System.out.println(banner);
+    }
+}
