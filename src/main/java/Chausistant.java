9bee12da302af072598dca002271f4b5253bc07b
chausarahnusitems-tech <chausarah.nusitems@gmail.com>
2026-08-18 17:03:52 +0800

    Level-1: Echo and deals with edge cases of bye

--- src/main/java/Chausistant.java
+++ src/main/java/Chausistant.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
+
 /**
  * Entry point for the Chausistant chatbot application.
  */
+
 public class Chausistant {
     public static void main(String[] args) {
+
         String banner = """
                  Hello! I'm 
                  
@@ -17,9 +21,23 @@ public class Chausistant {
                                       chausistant
                                       
                 What can I do for you today!  
-                -------------------------------------------------------------------
-                Bye. P(See you)! 
                 """;
         System.out.println(banner);
+
+        final String EXIT_COMMAND = "bye";
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (scanner.hasNextLine()) {
+                String input = scanner.nextLine();
+                String command = input.strip();
+
+                if (EXIT_COMMAND.equalsIgnoreCase(command)) {
+                    System.out.println("Bye. Hope to see you again soon!");
+                    break;
+                }
+
+                System.out.println(input + "\n---------------------");
+            }
+        }
     }
 }
